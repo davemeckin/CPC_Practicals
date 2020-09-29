06a07abefc95d27ca183e4dcd8424d9390f6fc3f
davemeckin <dmeckin@hotmail.com>
2020-09-29 09:57:18 +0100

    Update2

--- gh-pages-site/gatsby-config.js
+++ gh-pages-site/gatsby-config.js
@@ -41,7 +41,7 @@
 */
 
 module.exports = {
-  pathPrefix: "/CPC_Practicals",
+  pathPrefix: "/CPC_Practicals_Site",
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
