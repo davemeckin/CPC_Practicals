63278b9beaeaa73adbb2eba4fe1336b100294a6e
davemeckin <dmeckin@hotmail.com>
2020-09-30 16:05:41 +0100

    Updates

---
+++ react-syntax-highlighter_languages_refractor_ini-828b92023ba42b85c5e5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[71],{"22HQ":function(t,n,i){"use strict";function s(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=s,s.displayName="ini",s.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini-828b92023ba42b85c5e5.js.map
