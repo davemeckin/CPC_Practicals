0eb516b5c6ef41a8d0849a56a1aa328f92f4d0b3
davemeckin <dmeckin@hotmail.com>
2021-10-11 18:11:05 +0100

    Updates

---
+++ react-syntax-highlighter_languages_refractor_brainfuck-5bd7fafd78c3899b12b5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[40],{czuR:function(a,e,n){"use strict";function t(a){a.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}a.exports=t,t.displayName="brainfuck",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck-5bd7fafd78c3899b12b5.js.map
